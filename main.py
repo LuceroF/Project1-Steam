0be250b1f2bd038a03df5a458d38cd946961d258
Lucero Flores <lucero.flores.alfaro@gmail.com>
2024-01-24 18:10:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ from fastapi.staticfiles import StaticFiles
 #DATA GENERAL DE LA API
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="image"), name="static")
-app.title = "Juegos de Steam - Proyecto Individual N1"
+app.title = "Steam Games - Individual Project N1"
 app.description = """
 <div style="font-family: Arial, sans-serif; text-align: center;">
-    <img src="/static/Banner-deploy.jpg" alt="Banner de la aplicación">
+    <img src="/static/Banner-deploy.jpg" alt="Application Banner">
     <p style="font-size: 16px; color: #333;">
-        Bienvenidos a la aplicación de recomendación de juegos de Steam.
-        Aquí podrás explorar y descubrir juegos a través de nuestras
-        <strong>cinco funciones únicas</strong> y <strong>dos modelos de recomendación avanzados</strong>.
+        Welcome to the Steam game recommendation application.
+        Here you can explore and discover games through our
+        <strong>five unique features</strong> and <strong>two advanced recommendation models</strong>.
     </p>
     <p style="font-size: 14px; color: #555;">
-        Esta aplicación te ayudará a encontrar juegos según tus preferencias y te mostrará
-        recomendaciones basadas en análisis de datos complejos. ¡Explora y encuentra tu próximo juego favorito!
+        This application will help you find games according to your preferences and will show you
+        recommendations based on complex data analysis. Explore and find your next favorite game!
     </p>
 </div>
 """
@@ -37,37 +37,38 @@ funcion_4 = pd.read_parquet('data/funcion_4.parquet')
 funcion_5 = pd.read_parquet('data/funcion_5.parquet')
 modelo_recomendacion = pd.read_parquet('data/modelo_recomendacion.parquet')
 
-@app.get('/PlayTimeGenre/{genero}', tags=['Consultas sobre Steam Games'])
+@app.get('/PlayTimeGenre/{genero}', tags=['Inquiries about Steam Games'])
 def PlayTimeGenre(genero: str):
     """
     <font color="blue">
     <h1><u>PlayTimeGenre</u></h1>
     </font>
 
-    <b>Descubre el Año Estrella para tu Género de Juego Favorito.</b><br>
-    <b>¿Curioso por saber cuál año fue el más jugado para tu género favorito? Esta función te lo revela.</b><br>
+    <b>Discover the Star Year for Your Favorite Game Genre.</b><br>
+    <b>Curious to know which year was the most played for your favorite genre? This function reveals it to you.</b><br>
 
-    <em>Parámetros</em><br>
+    <em>Parameters</em><br>
     ----------
     genre : <code>str</code>
     
-        Introduce el género de juego en inglés, como "action", "simulation", "indie", etc.
+        Enter the game genre in English, such as "action", "simulation", "indie", etc.
 
-    <em>Resultado</em><br>
+    <em>Result</em><br>
     -----------
-    Ejemplo:
+    Example:
     ```python
         >>> MostPlayedYearForGenre("action")
     
-    {"Año con más horas jugadas para Género Action": 2014}
+    {"Year with Most Playtime for Action Genre": 2014}
     ```
-    PASOS PARA PROBAR<br>
-                            1. Selecciona "Try it out".<br>
-                            2. Escribe el género en el campo de texto, en minúsculas o mayúsculas, como prefieras (ejemplo: action).<br>
-                            3. Haz clic en "Execute".<br>
-                            4. Consulta el "Response body" para ver el año más jugado.
+    STEPS TO TEST<br>
+                            1. Select "Try it out".<br>
+                            2. Type the genre in the text field, in lowercase or uppercase, as you prefer (example: action).<br>
+                            3. Click on "Execute".<br>
+                            4. Check the "Response body" to see the most played year.
                             </font>
     """
+
 
     # Convertimos tanto el género de entrada como la columna del DataFrame a minúsculas
     genero_min = genero.lower()
@@ -81,44 +82,43 @@ def PlayTimeGenre(genero: str):
         max_playtime_row = genre_data.sort_values(by='MaxHours', ascending=False).iloc[0]
 
         # Devuelve el año de lanzamiento con más horas jugadas para el género dado
-        return {"Año de lanzamiento con más horas jugadas para Género " + genero: int(max_playtime_row["year_of_release"])}
+        return {"Year of Release with Most Playtime for Genre " + genero: int(max_playtime_row["year_of_release"])}
     else:
-        return {"Error": "Género no encontrado o sin datos"}
+        return {"Error": "Genre not found or without data"}
 
 
 def normalize_string(s):
     return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn').lower()
 
-@app.get("/UserForGenre/{genre}", tags=['Consultas sobre Steam Games'])
+@app.get("/UserForGenre/{genre}", tags=['Inquiries about Steam Games'])
 def UserForGenre(genre: str):
     """
     <font color="blue">
     <h1><u>UserForGenre</u></h1>
     </font>
 
-    <b>Explora Quién Domina en tu Género de Juegos Preferido.</b><br>
-    <b>Esta función desvela quién ha dedicado más horas a un género de juego en particular, acompañado de un historial detallado por año.</b><br>
+    <b>Explore Who Dominates in Your Favorite Game Genre.</b><br>
+    <b>This function reveals who has dedicated the most hours to a particular game genre, accompanied by a detailed yearly history.</b><br>
 
-    <em>Parámetros</em><br>
+    <em>Parameters</em><br>
     ----------
-    genero : <code>str</code>
-    
-        Escribe el género de juego en inglés, como "action", "simulation", "indie", etc.
+    genre : <code>str</code>
 
-    <em>Resultado</em><br>
+        Enter the game genre in English, such as "action", "simulation", "indie", etc.
+
+    <em>Result</em><br>
     -----------
-    Ejemplo:
+    Example:
     ```python
         >>> UserForGenre("action")
-    
-    {"Usuario": "player123", "con más horas jugadas para": "Action", "Historial acumulado": [{"Year": "2012", "Hours Played": 320}] }
-    ```
-    CÓMO PROBAR<br>
-                            1. Haz clic en "Try it out".<br>
-                            2. Introduce el género en el campo de texto (ejemplo: action).<br>
-                            3. Pulsa "Execute".<br>
-                            4. Navega hasta "Response body" para ver los detalles del usuario y su historial.
-                            </font>
+
+    {"User": "player123", "with most hours played for": "Action", "Accumulated History": [{"Year": "2012", "Hours Played": 320}] }
+    HOW TO TEST<br>
+                    1. Click on "Try it out".<br>
+                    2. Enter the genre in the text field (example: action).<br>
+                    3. Press "Execute".<br>
+                    4. Navigate to "Response body" to see the user details and their history.
+                    </font>
     """
 
     # Normalizar el género de entrada
@@ -139,37 +139,38 @@ def UserForGenre(genre: str):
     return {"Usuario": top_user, "con más horas jugadas para": genre, "Historial acumulado": user_history_dict}
 
 
-@app.get('/UsersRecommend/{year}', tags=['Consultas sobre Steam Games'])
+@app.get('/UsersRecommend/{year}', tags=['Inquiries about Steam Games'])
 def UsersRecommend(year: int):
     """
     <font color="blue">
     <h1><u>UsersRecommend</u></h1>
     </font>
 
-    <b>Los Juegos Más Valorados por los Usuarios para un Año Específico.</b><br>
-    <b>Descubre cuáles fueron los top 3 juegos más recomendados por los usuarios en un año concreto.</b><br>
+    <b>The Most Highly Rated Games by Users for a Specific Year.</b><br>
+    <b>Discover the top 3 games most recommended by users in a particular year.</b><br>
 
-    <em>Parámetros</em><br>
+    <em>Parameters</em><br>
     ----------
-    año : <code>int</code>
+    year : <code>int</code>
     
-        Año de interés, como 2010, 2012, etc.
+        The year of interest, such as 2010, 2012, etc.
 
-    <em>Resultado</em><br>
+    <em>Result</em><br>
     -----------
-    Ejemplo:
+    Example:
     ```python
         >>> UsersRecommend(2012)
     
-    [{"Puesto 1" : "Team Fortress 2"}, {"Puesto 2" : "Terraria"}, {"Puesto 3" : "Garry's Mod"}]
+    [{"1st Place" : "Team Fortress 2"}, {"2nd Place" : "Terraria"}, {"3rd Place" : "Garry's Mod"}]
     ```
-    CÓMO UTILIZAR<br>
-                            1. Haz clic en "Try it out".<br>
-                            2. Introduce el Año en el campo de abajo.<br>
-                            3. Pulsa "Execute".<br>
-                            4. Desplázate hasta "Response body" para descubrir los juegos más valorados.
-                            </font>
+    HOW TO USE<br>
+                        1. Click on "Try it out".<br>
+                        2. Enter the Year in the field below.<br>
+                        3. Press "Execute".<br>
+                        4. Scroll to "Response body" to find out the most highly rated games.
+    </font>
     """
+
     # Filtrar reseñas por el año dado, recomendaciones y análisis de sentimiento positivo o neutral
     filtered_reviews = funcion_3[
         (funcion_3['reviews_year'] == year) & 
@@ -195,96 +196,98 @@ def UsersRecommend(year: int):
     top_3_result = []
     for i, game in enumerate(top_3_games_list, start=1):
         game_info = {
-            f"Puesto {i}": game['app_name'],
-            "Recomendaciones": game['recommend_count']
+            f"Position {i}": game['app_name'],
+            "Recomendations": game['recommend_count']
         }
         top_3_result.append(game_info)
 
     if not top_3_result:
-        return f"No hay suficientes datos para juegos recomendados en el año {year}"
+        return f"Not enough data for recommended games in the year {year}"
     else:
-        response_message = f"Los 3 juegos MÁS recomendados por usuarios para el año {year} son:"
+        response_message = f"The top 3 MOST recommended games by users for the year {year} are:"
         return {response_message: top_3_result}
 
 
-@app.get('/UsersWorstDeveloper/{year}', tags=['Consultas sobre Steam Games'])
+@app.get('/UsersWorstDeveloper/{year}', tags=['Inquiries about Steam Games'])
 def UsersWorstDeveloper(year: int):
     """
     <font color="blue">
     <h1><u>UsersWorstDeveloper</u></h1>
     </font>
 
-    <b>Descubre los Desarrolladores de Juegos Menos Favoritos del Año.</b><br>
-    <b>Esta función identifica a los tres desarrolladores de juegos que han recibido más reseñas negativas en un año específico.</b><br>
+    <b>Discover the Least Favorite Game Developers of the Year.</b><br>
+    <b>This function identifies the three game developers who have received the most negative reviews in a specific year.</b><br>
 
-    <em>Parámetros</em><br>
+    <em>Parameters</em><br>
     ----------
-    año : <code>int</code>
+    year : <code>int</code>
     
-        Año de interés, por ejemplo: 2010, 2012, etc.
+        Year of interest, for example: 2010, 2012, etc.
 
-    <em>Resultado</em><br>
+    <em>Result</em><br>
     -----------
-    Ejemplo:
+    Example:
     ```python
         >>> UsersWorstDeveloper(2012)
     
-    [{"Puesto 1": "Desarrollador A"}, {"Puesto 2": "Desarrollador B"}, {"Puesto 3": "Desarrollador C"}]
+    [{"1st Place": "Developer A"}, {"2nd Place": "Developer B"}, {"3rd Place": "Developer C"}]
     ```
-    INSTRUCCIONES DE USO<br>
-                            1. Selecciona "Try it out".<br>
-                            2. Introduce el año en el campo de texto correspondiente.<br>
-                            3. Haz clic en "Execute".<br>
-                            4. Consulta el "Response body" para conocer a los desarrolladores menos favoritos.
-                            </font>
+    USAGE INSTRUCTIONS<br>
+                            1. Select "Try it out".<br>
+                            2. Enter the year in the corresponding text field.<br>
+                            3. Click on "Execute".<br>
+                            4. Check the "Response body" to find out the least favorite developers.
+    </font>
     """
+
     # Filtrar las reseñas para el año dado y donde la recomendación es False
     reviews_year = funcion_4[(funcion_4['reviews_year'] == year) & (funcion_4['reviews_recommend'] == False)]
 
     # Si no hay reseñas negativas para ese año, retorna mensaje
     if reviews_year.empty:
-        return f"No hay datos suficientes."
+        return f"Not enough data available."
 
     # Contar el número de reseñas negativas para cada desarrollador
     developer_negative_reviews = reviews_year['developer'].value_counts()
 
     # Preparar la salida para los tres primeros desarrolladores, si hay menos de tres, maneja la situación
     top_developers = developer_negative_reviews.head(3)
-    formatted_output = [{"Puesto {}".format(i + 1): dev} for i, dev in enumerate(top_developers.index)]
+    formatted_output = [{"Position {}".format(i + 1): dev} for i, dev in enumerate(top_developers.index)]
     
-    return f"Las 3 desarrolladoras con juegos MENOS recomendados por usuarios para el año {year}: {formatted_output}"
+    return f"The top 3 developers with the LEAST recommended games by users for the year {year}: {formatted_output}"
 
-@app.get('/sentiment_analysis/{developer_name}', tags=['Consultas sobre Steam Games'])
+@app.get('/sentiment_analysis/{developer_name}', tags=['Inquiries about Steam Games'])
 def sentiment_analysis(developer_name: str) -> str:
     """
     <font color="blue">
     <h1><u>SentimentAnalysis</u></h1>
     </font>
 
-    <b>Análisis de Sentimiento para Reseñas de Juegos de un Desarrollador Específico.</b><br>
-    <b>Explora la recepción general de los juegos de una desarrolladora, desglosando las reseñas en negativas, neutrales y positivas.</b><br>
+    <b>Sentiment Analysis for Game Reviews of a Specific Developer.</b><br>
+    <b>Explore the overall reception of a developer's games, breaking down the reviews into negative, neutral, and positive.</b><br>
 
-    <em>Parámetros</em><br>
+    <em>Parameters</em><br>
     ----------
     developer_name : <code>str</code>
     
-        Nombre de la empresa desarrolladora. Esta función es insensible a mayúsculas/minúsculas.
+        Name of the developer company. This function is case insensitive.
 
-    <em>Resultado</em><br>
+    <em>Result</em><br>
     -----------
-    Ejemplo:
+    Example:
     ```python
         >>> sentiment_analysis("Valve")
     
-    "El análisis de sentimiento de valor para la desarrolladora Valve es: [Negative = 10, Neutral = 15, Positive = 75]"
+    "The sentiment analysis value for developer Valve is: [Negative = 10, Neutral = 15, Positive = 75]"
     ```
-    INSTRUCCIONES DE USO<br>
-                            1. Haz clic en "Try it out".<br>
-                            2. Introduce el nombre de la desarrolladora en el campo de texto.<br>
-                            3. Haz clic en "Execute".<br>
-                            4. Consulta el "Response body" para ver el análisis de sentimiento.
-                            </font>
+    USAGE INSTRUCTIONS<br>
+                            1. Click on "Try it out".<br>
+                            2. Enter the developer's name in the text field.<br>
+                            3. Click on "Execute".<br>
+                            4. Check the "Response body" for the sentiment analysis.
+    </font>
     """
+
     # Normalizar el nombre de la desarrolladora
     developer_name_normalized = developer_name.lower()
 
@@ -298,47 +301,48 @@ def sentiment_analysis(developer_name: str) -> str:
                          "Positive": sentiment_counts.get(2, 0) }
 
     # Formateando el mensaje de salida
-    return f"El análisis de sentimiento de reseñas para la desarrolladora {developer_name} es: [Negative = {sentiment_counts['Negative']}, Neutral = {sentiment_counts['Neutral']}, Positive = {sentiment_counts['Positive']}]"
+    return f"The sentiment analysis of reviews for the developer {developer_name} is: [Negative = {sentiment_counts['Negative']}, Neutral = {sentiment_counts['Neutral']}, Positive = {sentiment_counts['Positive']}]"
 
 modelo_recomendacion = modelo_recomendacion.drop_duplicates(subset='item_id')
 
-@app.get("/recomendacion_juego/{item_id}", tags=['Sistema de Recomendación'])
+@app.get("/recomendacion_juego/{item_id}", tags=['Recommendation System'])
 def  recomendacion_juego(item_id:int):
     """
     <font color="blue">
-    <h1><u>Recomendación de Juegos</u></h1>
+    <h1><u>Game Recommendation</u></h1>
     </font>
 
-    <b>Recomendaciones de juegos basadas en un sistema de recomendación item-item.</b><br>
-    <b>Obtén una lista de juegos similares basada en un juego específico proporcionado por su ID.</b><br>
+    <b>Game recommendations based on an item-item recommendation system.</b><br>
+    <b>Get a list of similar games based on a specific game provided by its ID.</b><br>
 
-    <em>Parámetros</em><br>
+    <em>Parameters</em><br>
     ----------
     item_id : <code>int</code>
     
-        El ID numérico del juego. Esta función utiliza el ID para encontrar juegos similares.
+        The numeric ID of the game. This function uses the ID to find similar games.
 
-    <em>Resultado</em><br>
+    <em>Result</em><br>
     -----------
-    Ejemplo:
+    Example:
     ```python
-        >>> recomendacion_juego(12345)
+        >>> game_recommendation(12345)
     
-    "Lista de juegos recomendados similares para 12345."
+    "List of similar recommended games for 12345."
     ```
-    INSTRUCCIONES DE USO<br>
-                            1. Haz clic en "Try it out".<br>
-                            2. Introduce el ID del juego en el campo de texto.<br>
-                            3. Haz clic en "Execute".<br>
-                            4. Consulta el "Response body" para ver la lista de juegos recomendados.
-                            </font>
+    USAGE INSTRUCTIONS<br>
+                            1. Click on "Try it out".<br>
+                            2. Enter the game ID in the text field.<br>
+                            3. Click on "Execute".<br>
+                            4. Check the "Response body" to see the list of recommended games.
+    </font>
     """
+
     
     try:
         game = modelo_recomendacion[modelo_recomendacion['item_id'] == item_id]
     
         if game.empty:
-            return f"El juego '{item_id}' no posee registros." 
+            return f"The game '{item_id}' does not have any records."
     
         # Obtenemos el índice del juego dado
         idx = game.index[0]
@@ -365,11 +369,11 @@ def  recomendacion_juego(item_id:int):
 
         # Formatear el mensaje de salida para que solo incluya juegos únicos
         respuesta = {
-        "Lista de juegos recomendados similares para": game['item_name'].iloc[0],
-        "Juegos Recomendados": unique_similar_games
+        "List of similar recommended games for": game['item_name'].iloc[0],
+        "Recommended Games": unique_similar_games
         }
 
         return respuesta
     except Exception as e:
         # Manejo genérico de excepciones, puedes especificar errores más específicos si lo prefieres
-        raise HTTPException(status_code=500, detail="El ID del juego debe ser un número entero.")
+        raise HTTPException(status_code=500, detail="The game ID must be an integer number.")
